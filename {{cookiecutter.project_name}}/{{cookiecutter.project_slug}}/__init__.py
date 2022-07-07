75c3f11c5ebc69340be97874ed2935a39cc32382
Mattia Almansi <m.almansi@bopen.eu>
2022-07-07 10:47:49 +0200

    add pydocstyle

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,3 +1,5 @@
+"""{{ cookiecutter.project_short_description }}."""
+
 # Copyright {{ cookiecutter.copyright_year }}, {{ cookiecutter.copyright_holder }}.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
